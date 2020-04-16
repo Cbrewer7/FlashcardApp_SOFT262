6a48f6e2c445a3c367f16e05c0fc4a450e9b3cdd
Casey <Kusella-Gussin>
2020-04-16 17:32:18 +0100

    Azure Server
    The program is now connected to the Azure server

---
+++ FlashcardsApp/AzureDB/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace AzureDB
+{
+    class Program
+    {       
+
+        private static readonly string endPointUri = "https://ckgussin.documents.azure.com:443/";
+
+        private static readonly string PrimaryKey = "9tPlkGpHZBGXov6kIEJRaFdZTaN0uIKqhKINIsICv7pNUr2CGY6UrXoJPu8Oj7QzaCEyJximAanmQdio5tnGkg==";
+
+        private CosmosClient cosmosClient;
+
+        private Database database;
+
+        private Container container;
+
+        private string databaseName = "flashCardsDB";
+        private string containerId = "Items";
+
+        public static async Task Main(string[] args)
+        {
+            var p = new Program();
+            await p.Start();
+
+        
+        }
+        
+
+        public async Task Start()
+        {
+            this.cosmosClient = new CosmosClient(endPointUri, PrimaryKey, new CosmosClientOptions()
+            {
+                ApplicationName = "Subjects of FlashCards"
+
+            });
+
+            //Creates a new database within Azure server
+            this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseName);
+            Console.WriteLine("Created the database: {0}\n", this.database.Id);
+
+            //Creates Container
+            this.container = await this.database.CreateContainerIfNotExistsAsync(containerId, "/Topics", 400);
+
+            //Adds records (Need to link this to creating the flashcard in the app)
+           
+        }
+
+    }
+}
--- FlashcardsApp/FlashcardsApp/FlashcardsApp/MainPage.xaml.cs
+++ FlashcardsApp/FlashcardsApp/FlashcardsApp/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Microsoft.Azure.Cosmos;
+
 
 namespace FlashcardsApp
 {
@@ -17,5 +19,7 @@ namespace FlashcardsApp
         {
             InitializeComponent();
         }
+
+        
     }
 }
